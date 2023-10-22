f9227738f00881bf5777ec05a43b7b7be92f0e0b
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2023-10-22 11:04:30 +0200

    Adds link to news site

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,6 +1,8 @@
 <header>
     <h1>Wingfoil Gear Talk</h1>
     <ul class="nav">
+        <li><a class="nav-link" href="/">Reports</a></li>
+        <li><a class="nav-link" target="_blank" href="https://news.wingbuddies.de">News</a></li>
         <li><a class="nav-link" href="/about">About</a></li>
     </ul>
     <hr>
