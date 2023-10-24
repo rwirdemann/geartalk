86ad3305424a3705de3974a683bf2e4ade993c0a
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2023-10-24 09:20:08 +0200

    Adds basic create report form

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,6 +2,7 @@
     <h1>Wingfoil Gear Talk</h1>
     <ul class="nav">
         <li><a class="nav-link" href="/">Reports</a></li>
+        <li><a class="nav-link" href="/createreport">Submit Report...</a></li>
         <li><a class="nav-link" target="_blank" href="https://news.wingbuddies.de">News</a></li>
         <li><a class="nav-link" href="/about">About</a></li>
     </ul>
---
+++ src/routes/createreport/+page.svelte
@@ -0,0 +1,55 @@
+<form class="container-md">
+    <div class="row">
+        <div class="col">
+            <div class="mb-3">
+                <label class="form-label">Report über</label>
+                <select class="form-select">
+                    <option>Board</option>
+                    <option>Wing</option>
+                    <option>Mast</option>
+                    <option>Frontwing</option>
+                    <option>Rearwing</option>
+                </select>
+            </div>
+        </div>
+        <div class="col">
+            <div class="mb-3">
+                <label class="form-label">Hersteller</label>
+                <div class="input-group">
+                    <select class="form-select">
+                        <option>Fanatic</option>
+                        <option>Gong</option>
+                        <option>Appletree</option>
+                        <option>Vayu</option>
+                    </select>
+                    <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        Neu...
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="mb-3">
+            <label for="description" class="form-label">Dein Bericht</label>
+            <textarea class="form-control" id="description" rows="5"></textarea>
+        </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Report einstellen</button>
+</form>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
